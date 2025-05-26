d9eaaf3c5f1c7f7fa995fa3730c690afc667fa92
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2025-05-26 12:03:04 -0300

    conteudo da aula do dia 26/05/2025

---
+++ teste.js
@@ -0,0 +1,75 @@
+const leia = require('readline-sync')
+// Variaveis JavaScript
+
+// Java
+// int -> numeros inteiro
+// float -> numeros flutuantes (casa decimal)
+// bigint -> numeros inteiros grandes
+// double -> numeros flutuantes (casa decimal) grandes
+
+//JS -> number
+
+// Java
+// char -> armazena um unico caractere
+// String -> armazena uma lista de caracteres
+
+// JS
+// string -> qualquer tipo de texto, ou caracteres
+
+// let nome = 'O "Thiago" ta com sono';
+// let nome2 = "Thiago"
+// let opcao = false
+
+// criando variavel sem valor
+// let batata
+// console.log(batata)
+
+// console.log(157 * "b")
+
+// Padrão camelCase
+// let nome_completo -> Snake_Case
+// let nome-completo -> kebab-case
+// let nomeCompleto = 'camelCase'
+// var segundoNome = ''
+
+// let nota1 = 5, nota2 = 3, nota3 = 8, nota4
+
+// console.log("A variavel nome completo é do tipo: ", typeof(nomeCompleto) )
+
+// const valor = 8
+
+// console.log(valor)
+
+
+// let identificador = 10000;
+// let identificador2 = 20000;
+// let identificador3 = 30000;
+// let area = 45.4567;
+// let palavra = "Generation";
+
+// console.log("Formatação - Número  Inteiro: \rprimeiro: %d ,segundo: %d ,terceiro: %d", identificador, identificador2, identificador3);
+// console.log("Formatação - Número Inteiro: primeiro: " + identificador + " ,segundo: " + identificador2)
+// console.log(`Aqui é um texto que vai mostrar um numero:\t ${identificador},${palavra} e continua o texto`);
+// console.log("Formatação - Número Real: %f",area.toFixed(2));
+// console.log("Formatação - String: %s",palavra);
+
+// let salarioBruto = 3500.44845
+// console.log("Formatação - Valor Monetário:\t " +
+//   new Intl.NumberFormat('en-US', {
+//     style: 'currency',
+//     currency: 'BRL',
+//   }).format(salarioBruto))
+  
+  
+// let nomeCompleto = leia.question('Digite o seu nome completo: ')
+// console.log("Seu nome completo é: " + nomeCompleto);
+
+
+// let quantidade
+
+
+
+// quantidade = leia.questionInt("\nDigite um valor do tipo Inteiro: ", {limitMessage: 'Digite um numero inteiro'});
+// console.log("O valor inteiro digitado foi: " + quantidade);
+
+
